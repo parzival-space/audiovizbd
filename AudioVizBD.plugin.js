c72a6f996dd68e3cb3a5bee40b7c6b91954e657e
Malte L <38286424+parzival-games@users.noreply.github.com>
2020-09-05 02:44:09 +0200

    fixed some stuff

--- AudioVizBD.plugin.js
+++ AudioVizBD.plugin.js
@@ -1,10 +1,9 @@
 /**
  * @name AudioVizBD
- * @version 0.0.5
+ * @version 1.0.2
  * @description Adds an audio visualizer behind the user modal.
  * @authorLink http://cyberfen.eu/discord
  * @donate http://cyberfen.eu/donate
- * @steam http://cyberfen.eu/steam
  * @source https://cyberfen.github.io/audiovizbd/AudioVizBD.plugin.js
  */
 
@@ -16,7 +15,7 @@ var AudioVizBD = (() => {
             return "AudioVizBD";
         }
         getVersion() {
-            return "1.0.0";
+            return "1.0.2";
         }
         getAuthor() {
             return "𝓟𝓪𝓻𝔃𝓲𝓿𝓪𝓵";
@@ -29,15 +28,41 @@ var AudioVizBD = (() => {
         constructor() {}
 
         // Load Plugin
-        load() {}
+        load() {
+            if (BdApi.getData(this.getName(), "colorCode") == undefined) BdApi.setData(this.getName(), "ColorCode", "#7289da");
+            this.ColorCode = BdApi.getData(this.getName(), "colorCode");
+
+            if (BdApi.getData(this.getName(), "hasShownChangelog") != true) {
+                ZeresPluginLibrary.Modals.showChangelogModal(this.getName(), this.getVersion(), Changelog, "View [GitHub Repo](https://github.com/cyberfen/audiovizbd)");
+                BdApi.setData(this.getName(), "hasShownChangelog", true)
+            }
+        }
+
+        // Settings
+        getSettingsPanel() {
+            let settings = document.createElement("div");
+            settings.style.padding = "10px";
+
+            let title = document.createElement("h1");
+            title.innerText = this.getName();
+            settings.appendChild(title)
+
+            settings.appendChild(new ZeresPluginLibrary.Settings.ColorPicker("Color", "Can be overwritten by themes.", BdApi.getData(this.getName(), "colorCode"), (color) => {
+                BdApi.setData(this.getName(), "colorCode", color);
+                this.ColorCode = color;
+            }).getElement());
+
+            return settings;
+        }
 
         // Start
         start() {
-            setTimeout(() => {
-                this.intervals = [];
-                this.startVisualizer();
-                this.loadStylesheet('https://cyberfen.github.io/audiovizbd/visualizer.css');
-            }, 0)
+            if (!global.ZeresPluginLibrary) return window.BdApi.alert("Library Missing", `The library plugin needed for ${this.getName()} is missing.<br /><br /> <a href="https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js" target="_blank">Click here to download the library!</a>`);
+            ZLibrary.PluginUpdater.checkForUpdate(this.getName(), this.getVersion(), "https://cyberfen.github.io/audiovizbd/AudioVizBD.plugin.js");
+
+            this.intervals = [];
+            this.startVisualizer();
+            this.loadStylesheet('https://cyberfen.github.io/audiovizbd/visualizer.css');
         }
 
         // Reload
@@ -51,20 +76,24 @@ var AudioVizBD = (() => {
             for (const interval of this.intervals) {
                 clearInterval(interval);
             }
+            let visualizer = document.querySelector('.audioviz-visualizer');
+            if (visualizer) visualizer.remove();
         }
 
         //===========================================//
-        //                Own Plugin                 //
-        //===========================================//
 
         loadStylesheet(url) {
-            $('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', url));
+            var loader = document.createElement("link");
+            loader.rel = "stylesheet";
+            loader.href = url;
+            document.getElementsByTagName("head")[0].appendChild(loader);
         }
 
         stopVisualizer() {
             for (const interval of this.intervals) {
                 clearInterval(interval);
             }
+            document.querySelector('.audioviz-visualizer').remove();
         }
 
         startVisualizer() {
@@ -85,6 +114,16 @@ var AudioVizBD = (() => {
                             chromeMediaSource: 'desktop'
                         }
                     }
+                }).catch((err) => {
+                    ZeresPluginLibrary.Modals.showConfirmationModal(this.getName() + " - Oops...",
+                    "Something didnt work as expected:<br>**" + err + "**\n\nIf you see this message the first time,\n try to reload your discord.\n\nIf the problem still persists, please open an issue: https://github.com/cyberfen/audiovizbd/issues",
+                    {
+                        danger: true,
+                        confirmText: "Reload",
+                        cancelText: "Ignore",
+                        onConfirm: function() {DiscordNative.app.relaunch();},
+                        onCancel: function() {}
+                    });
                 })
 
                 const audioCtx = new AudioContext()
@@ -100,7 +139,7 @@ var AudioVizBD = (() => {
                 visualizer.classList.add('audioviz-visualizer')
                 for (let i = 0; i < barCount; i++) {
                     let bar = document.createElement('div')
-                    bar.classList.add('audioviz-bar')
+                    bar.classList.add('audioVisualizer')
                     bar.style.height = Math.round(Math.random() * 90) + 5 + 'px'
                     visualizer.appendChild(bar)
                 }
@@ -109,18 +148,18 @@ var AudioVizBD = (() => {
                 visualizerGoo.setAttributeNS('http://www.w3.org/2000/version/', 'version', '1.1')
                 visualizerGoo.classList.add('audioviz-goo')
                 visualizerGoo.innerHTML = `
-                <filter id="audioVisualizer">
+                <filter id="audiovizGoo">
                   <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur"></feGaussianBlur>
-                  <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="audioVisualizer"></feColorMatrix>
-                  <feComposite in="SourceGraphic" in2="audioVisualizer" operator="atop"></feComposite>
+                  <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="audiovizGoo"></feColorMatrix>
+                  <feComposite in="SourceGraphic" in2="audiovizGoo" operator="atop">e</feComposite>
                 </filter>
               `
 
                 const findElement = setInterval(() => {
                     if (accountContainer) {
-                        visualizer = document.querySelector('.audioviz-visualizer')
+                        visualizer = document.querySelector('.audioviz-visualizer');
                     } else {
-                        accountContainer = document.querySelector('.panels-j1Uci_ > .container-3baos1:last-child')
+                        accountContainer = document.getElementsByClassName(BdApi.findModuleByProps('downloadProgressCircle', 'panels').panels)[0].lastElementChild;
                         if (accountContainer) {
                             accountContainer.prepend(visualizer)
                             accountContainer.prepend(visualizerGoo)
@@ -139,6 +178,9 @@ var AudioVizBD = (() => {
                         const height = easeInOutCubic(Math.min(1, y / 255)) * 100 + 50
                         const bar = visualizer.children[i]
                         bar.style.height = height + '%'
+                        if (bar.style.backgroundColor != this.ColorCode) {
+                            this.style.backgroundColor = this.ColorCode;
+                        }
                     }
                 }, 20)
                 this.intervals = [style, findElement]
@@ -146,6 +188,23 @@ var AudioVizBD = (() => {
                 console.error('An error occurred getting media sources', error)
             })
         }
-
     }
 })();
+
+const Changelog = [
+    {
+        title: "Customization :)",
+        type: "added",
+        items: [
+            "Now you can change the color of the visualizer in the settings."
+        ]
+    },
+    {
+        title: "Bye JQuery",
+        type: "improved",
+        items: [
+            "Removed JQuery",
+            "Now using WebpackModule form classnames. thx to @Strencher#1044 "
+        ]
+    }
+]
